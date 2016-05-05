f49c81123cbcdfb5fc5993c9140ed6ccc567ca94
Saghi Nojoomi <sgnojoomi@ucdavis.edu>
2016-05-05 15:16:57 -0700

    App_Amanda-Brianna-Saghi/ui.R

--- App_Amanda-Brianna-Saghi/ui.R
+++ App_Amanda-Brianna-Saghi/ui.R
@@ -17,8 +17,8 @@ shinyUI(fluidPage( #create the overall page
   titlePanel("Rice Data"),
   
   # Some helpful information
-  helpText("This application creates a boxplot to show difference between",
-           "iris species.  Please use the radio box below to choose a trait",
+  helpText("This application create a density plots to show difference between",
+           "Rice species.  Please use the radio box below to choose a trait",
            "for plotting"),
   
   # Sidebar with a radio box to input which trait will be plotted
